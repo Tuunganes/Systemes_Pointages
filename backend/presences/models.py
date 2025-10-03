3f45a018f2e451d98f857f77abce7c5b7a3f59ec
Prospershabani03@gmail.com <prospershabani03@gmail.com>
2025-10-03 18:36:12 +0200

    ajout de table presences,et initialisation de migration

--- backend/presences/models.py
+++ backend/presences/models.py
@@ -1,3 +1,21 @@
+from employes.models import Employe
 from django.db import models
 
-# Create your models here.
+class Presence(models.Model):
+    employe = models.ForeignKey(
+        Employe,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='presences'
+    )
+    date_presence = models.DateField()
+    heure_entree = models.TimeField()
+    heure_sortie = models.TimeField(null=True, blank=True)
+    statut = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+       if self.employe:
+        return f"{self.employe.nom} - {self.date_presence}"
+       else:
+        return f"Employé supprimé - {self.date_presence}"
